61b5d113d696804c12c740cdd310bbdc68127bc5
ZAPE <sangeeth999123@gmail.com>
2025-01-29 15:27:42 +0530

    Update db.js added the await

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
-// import mongoose from 'mongoose';
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI );
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
-    
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1); 
   }
 };
 
