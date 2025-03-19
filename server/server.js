9690b52f28fef9aac9f01f1c282414e9bc5dd769
sangeeth-606 <sangeeth999123@gmail.com>
2025-03-19 14:55:15 +0530

    Refactor server setup to improve structure and integrate Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,32 +1,74 @@
-const express =require('express');
-const mongoose = require('mongoose'); 
+// const express =require('express');
+// const mongoose = require('mongoose'); 
+
+// const roomRoutes = require('./Routes/roomRoutes');
+// const connectDB = require('./config/db');
+// const setupSocket = require('./config/socket')
+
+
+// const app = express();
+
+// const server=http.createServer(app)
+// const io = setupSocket(server)
+// const port = 3000;
+
+
+// require('dotenv').config();
+// connectDB();
+
+
+// app.use(express.json())
+// app.use('/',roomRoutes)
+
+
+// app.post('/ping',(req,res)=>{
+//     res.send('pong')
+// })
+
+// app.get('/', (req, res) => {
+//     try {
+//         const dbStatus = mongoose.connection.readyState;
+//         const statusMessage = dbStatus === 1 ? 'Database Connected' : 'Database Not Connected';
+//         res.send(statusMessage);
+//     } catch (error) {
+//         res.status(500).send('Error checking database status');
+//     }
+//      });
+// app.listen(port,()=>{
+//     console.log(`Server is running on port ${port}`)
+// })
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
 
 const roomRoutes = require('./Routes/roomRoutes');
-const app = express();
-const port = 3000;
 const connectDB = require('./config/db');
+const setupSocket = require('./socket'); // Import setupSocket
+
+const app = express();
+const server = http.createServer(app); // Create HTTP server
+const io = setupSocket(server); // Initialize Socket.IO
 
 require('dotenv').config();
 connectDB();
 
+app.use(express.json());
+app.use('/', roomRoutes);
 
-app.use(express.json())
-app.use('/',roomRoutes)
-
-
-app.post('/ping',(req,res)=>{
-    res.send('pong')
-})
+app.post('/ping', (req, res) => {
+  res.send('pong');
+});
 
 app.get('/', (req, res) => {
-    try {
-        const dbStatus = mongoose.connection.readyState;
-        const statusMessage = dbStatus === 1 ? 'Database Connected' : 'Database Not Connected';
-        res.send(statusMessage);
-    } catch (error) {
-        res.status(500).send('Error checking database status');
-    }
-     });
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+  try {
+    const dbStatus = mongoose.connection.readyState;
+    const statusMessage = dbStatus === 1 ? 'Database Connected' : 'Database Not Connected';
+    res.send(statusMessage);
+  } catch (error) {
+    res.status(500).send('Error checking database status');
+  }
+});
+
+server.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
