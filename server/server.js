6e0b06367f6112b92a925f177d85d1c35d6dbace
sangeeth-606 <sangeeth999123@gmail.com>
2025-03-25 21:31:02 +0530

    td

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const connectDB = require('./config/db');
 const setupSocket = require('./config/socket');
 
 const app = express();
+
 const server = http.createServer(app);  // Create HTTP server
 const io = setupSocket(server); // Attach Socket.io to the server
 
